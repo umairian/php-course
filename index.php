968eba506b3ba064f4f72347cfff5f462bbc6e74
Umair Syed <itsumairsyed@gmail.com>
2020-07-18 00:49:28 -0700

    initial php setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+      echo "<h1>Welcome!</h1>";
+     ?>
+  </body>
+</html>
